a627cd431546ca57b1fb7299cd70741f36a4508c
Jack Schofield <59843981+jschof1@users.noreply.github.com>
2023-10-31 16:27:43 +0000

    add check for expected start prompo

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
-const express = require('express');
-const OpenAI = require('openai');
-const cors = require('cors');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
+const express = require("express");
+const OpenAI = require("openai");
+const cors = require("cors");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 require("dotenv").config();
 
-const OPENAI_API_KEY = process.env.OPENAI_API_KEY
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-
-const openai = new OpenAI({apiKey: OPENAI_API_KEY});
+const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(morgan('dev'));  // Logging middleware
+app.use(morgan("dev")); // Logging middleware
 
-// Endpoint for OpenAI completions
+const EXPECTED_START =
+  "Anticipate and address the potential impacts of a product or service on society.";
 
-app.post('/openai-completion', async (req, res) => {
-    console.log('Received request:', req.body);
-    try {
-      const response = await openai.chat.completions.create({
-        ...req.body,
-      });
-     res.status(200).send(response.data || response);
-    } catch (error) {
-        console.error('Error in /openai-completion route:', error);
-        res.status(error.status).json({ error: error.name });
-    }
+app.post("/openai-completion", async (req, res) => {
+  console.log("Received request:", req.body);
+  if (!req.body.prompt || !req.body.prompt.startsWith(EXPECTED_START)) {
+    return res.status(400).json({ error: "Invalid prompt content." });
+  }
+  try {
+    const response = await openai.chat.completions.create({
+      ...req.body,
+    });
+    res.status(200).send(response.data || response);
+  } catch (error) {
+    console.error("Error in /openai-completion route:", error);
+    res.status(error.status).json({ error: error.name });
+  }
 });
 
-
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
